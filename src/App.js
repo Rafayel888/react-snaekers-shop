d09d82edb268966e70da76797c54a958392e3540
Rafayel <rafayelmkhitaryan303@gmail.com>
2023-02-13 23:53:30 +0400

    start

---
+++ src/App.js
@@ -0,0 +1,219 @@
+
+
+function App() {
+  return (
+    <div className="wrapper-container">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img src="/img/icons/logo.svg" width={40} height={40} />
+          <div className="headerInfol">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+       </div>
+        
+        <ul className="d-flex">
+            <li className="mr-30"><img width={18} height={18} src="/img/icons/basket.svg" /><span>1205 руб.</span></li>
+            <li><img width={20} height={18} src="/img/icons/heart.svg"/></li>
+            <li><img width={20} height={20} src="/img/icons/user.svg"/></li>
+          </ul>
+        
+      </header>
+    
+      <div className="content p-40">
+        <h1 className="mb-40">Все кроссовки</h1>
+
+        <div className="sneakers">
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_1.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_2.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Air Max 270</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_3.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>8 499 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_4.jpg" alt="Sneakers" />
+            <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>8 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_5.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Under Armour Curry 8</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>15 199 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_6.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Kyrie 7</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>11 299 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_7.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Jordan Air Jordan 11</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>10 799 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_8.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Jordan Air Jordan 11</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>16 499 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_9.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Lebron XVIII Low</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>13 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_1.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>8 499 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_11.png" alt="Sneakers" />
+            <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>8 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          <div className="card-see">
+            <img className="card-img" src="/img/sneak_img/sneak_12.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Kyrie Flytrap IV</h5>
+            <div className="d-flex justify-between align-center mt-15">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>11 299 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/icons/plus.svg" />
+              </button>
+            </div>
+
+          </div>
+
+          
+
+          
+         
+
+         
+       
+       </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './reset.css';
+import 'macro-css';
+import './index.scss';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
